5ffd621f55798d237be93d4ede14390ca193ff77
T45K <tasktas9@gmail.com>
2023-03-05 22:44:50 +0900

    だいたい理解した

--- main.go
+++ main.go
@@ -1,10 +1,19 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"reflect"
+	"strconv"
+)
 
 func main() {
 	route := gin.Default()
 	route.GET("/ping", handlePing)
+	route.GET("/query", handleQuery)
+	route.GET("/param/:name/:id", handlePathParam)
+	route.POST("/ping", receivePing)
+	route.POST("/ping2", receivePing2)
 	route.Run()
 }
 
@@ -12,4 +21,47 @@ func handlePing(c *gin.Context) {
 	c.JSON(200, gin.H{
 		"message": "pong",
 	})
+}
+
+func handleQuery(c *gin.Context) {
+	name := c.Query("name")
+	unknown := c.Query("unknown")
+	c.JSON(200, gin.H{"name": name, "unknown": unknown})
+}
+
+func handlePathParam(c *gin.Context) {
+	name := c.Param("name")
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.AbortWithStatus(400)
+		return
+	}
+	c.JSON(200, gin.H{"name": name, "id": id})
+}
+
+func receivePing(c *gin.Context) {
+	var body PingRequestBody
+	if err := c.ShouldBindJSON(&body); err != nil {
+		c.AbortWithError(400, err)
+		return
+	}
+	fmt.Println(body)
+	c.JSON(200, gin.H{"message": "success"})
+}
+
+type PingRequestBody struct {
+	Name string
+	Id   int
+}
+
+// もう一つの方法
+func receivePing2(c *gin.Context) {
+	var body map[string]interface{}
+	if err := c.ShouldBindJSON(&body); err != nil {
+		c.AbortWithError(400, err)
+		return
+	}
+	fmt.Println(body)
+	fmt.Println(reflect.TypeOf(body["name"]))
+	c.JSON(200, gin.H{"message": "success"})
 }
